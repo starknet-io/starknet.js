e77f58a427902332a06417f46aca2366e886ca06
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2021-11-29 14:43:57 +0100

    chore: rename some instances of 'tx' to 'transaction'

--- src/signer/interface.ts
+++ src/signer/interface.ts
@@ -8,8 +8,10 @@ export abstract class SignerInterface extends Provider {
    *
    * [Reference](https://github.com/starkware-libs/cairo-lang/blob/f464ec4797361b6be8989e36e02ec690e74ef285/src/starkware/starknet/services/api/gateway/gateway_client.py#L13-L17)
    *
-   * @param tx - transaction to be invoked
+   * @param transaction - transaction to be invoked
    * @returns a confirmation of invoking a function on the starknet contract
    */
-  public abstract override addTransaction(tx: Transaction): Promise<AddTransactionResponse>;
+  public abstract override addTransaction(
+    transaction: Transaction
+  ): Promise<AddTransactionResponse>;
 }
