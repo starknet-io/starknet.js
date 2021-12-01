e5bdb1890e0bc53b03d4b145069cf7fbc639e830
Janek Rahrt <janek.rahrt@me.com>
2021-12-01 14:11:33 +0100

    feat: add compileStructToCalldata

--- src/utils/stark.ts
+++ src/utils/stark.ts
@@ -49,3 +49,7 @@ export function formatSignature(sig?: [BigNumberish, BigNumberish]): [string, st
     return [];
   }
 }
+
+export function compileStructToCalldata<S extends { [k: string]: string }>(struct: S): string[] {
+  return Object.values(struct);
+}
