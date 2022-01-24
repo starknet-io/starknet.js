8bd39b9dd1611c57f7c09197a43bd85a2f906cd2
dhruvkelawala <dhruvrk2000@gmail.com>
2022-01-24 17:54:56 +0530

    chore(address): add padding to parsed address

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,14 +1,16 @@
+import { addHexPrefix, removeHexPrefix } from './encode';
+
 export function validateAndParseAddress(address: string): string {
   let result = null;
   if (typeof address !== 'string') {
     throw new Error('Invalid Address Type');
   }
 
-  if (!address.match(/^(0x)?[0-9a-fA-F]{63,64}$/)) {
+  if (address.match(/^(0x)?[0-9a-fA-F]{63, 64}$/)) {
     throw new Error('Invalid Address Format');
   }
 
-  result = address.substring(0, 2) === '0x' ? address : `0x${address}`;
+  result = addHexPrefix(removeHexPrefix(address).padStart(64, '0'));
 
   return result;
 }
