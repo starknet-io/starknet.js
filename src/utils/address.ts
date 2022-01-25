7486944b45e78d62208fe6325f2dd57c3bcef748
dhruvkelawala <dhruvrk2000@gmail.com>
2022-01-25 16:43:02 +0530

    chore(address): update validateAndParseAddress

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,4 +1,10 @@
+import { MASK_251, ZERO } from '../constants';
 import { addHexPrefix, removeHexPrefix } from './encode';
+import { assertInRange } from './number';
+
+export function addAddressPadding(address: string): string {
+  return addHexPrefix(removeHexPrefix(address).padStart(64, '0'));
+}
 
 export function validateAndParseAddress(address: string): string {
   let result = null;
@@ -6,11 +12,13 @@ export function validateAndParseAddress(address: string): string {
     throw new Error('Invalid Address Type');
   }
 
-  if (address.match(/^(0x)?[0-9a-fA-F]{63, 64}$/)) {
+  assertInRange(address, ZERO, MASK_251, 'Starknet Address');
+
+  result = addAddressPadding(address);
+
+  if (!result.match(/^(0x)?[0-9a-fA-F]{64}$/)) {
     throw new Error('Invalid Address Format');
   }
-
-  result = addHexPrefix(removeHexPrefix(address).padStart(64, '0'));
 
   return result;
 }
