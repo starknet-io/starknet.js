494e0005c3b1cb86f5de0f25a10cbbe786a87b34
Sean <seanhan94@gmail.com>
2022-03-09 09:12:53 -0500

    fix: link

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/guides/intro">
             Get Started - 5 min ⏱️
           </Link>
         </div>
