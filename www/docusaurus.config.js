df9cc1aa9b78f2868a251b863b9d62af4260a777
Sean <seanhan94@gmail.com>
2022-03-16 19:59:59 -0400

    remove blog

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
             label: 'Guides',
             to: '/guides/intro'
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
