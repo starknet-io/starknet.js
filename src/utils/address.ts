2d32ed828f908090642a42d9f6620f050a75b43a
Janek Rahrt <janek.rahrt@me.com>
2022-05-04 15:16:30 +0200

    feat: add checksum addresses

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,16 +1,16 @@
+/* eslint-disable no-bitwise */
+import { arrayify } from '@ethersproject/bytes';
+
 import { MASK_251, ZERO } from '../constants';
 import { addHexPrefix, removeHexPrefix } from './encode';
-import { assertInRange } from './number';
+import { pedersen } from './hash';
+import { BigNumberish, assertInRange, toBN, toHex } from './number';
 
-export function addAddressPadding(address: string): string {
-  return addHexPrefix(removeHexPrefix(address).padStart(64, '0'));
+export function addAddressPadding(address: BigNumberish): string {
+  return addHexPrefix(removeHexPrefix(toHex(toBN(address))).padStart(64, '0'));
 }
 
-export function validateAndParseAddress(address: string): string {
-  if (typeof address !== 'string') {
-    throw new Error('Invalid Address Type');
-  }
-
+export function validateAndParseAddress(address: BigNumberish): string {
   assertInRange(address, ZERO, MASK_251, 'Starknet Address');
 
   const result = addAddressPadding(address);
@@ -21,3 +21,23 @@ export function validateAndParseAddress(address: string): string {
 
   return result;
 }
+
+export const getChecksumAddress = (address: BigNumberish): string => {
+  const chars = removeHexPrefix(validateAndParseAddress(address)).toLowerCase().split('');
+  const hashed = arrayify(pedersen([0, address]), { hexPad: 'left' }); // as the hash will be 251 bits (63 chars) we need to pad it to 64 chars without changing the number value ("left")
+
+  for (let i = 0; i < chars.length; i += 2) {
+    if (hashed[i >> 1] >> 4 >= 8) {
+      chars[i] = chars[i].toUpperCase();
+    }
+    if ((hashed[i >> 1] & 0x0f) >= 8) {
+      chars[i + 1] = chars[i + 1].toUpperCase();
+    }
+  }
+
+  return addHexPrefix(chars.join(''));
+};
+
+export const validateChecksumAddress = (address: string): boolean => {
+  return getChecksumAddress(address) === address;
+};
