e9b8674f51c6d6fe57772a4c88b162c5132cfbdd
Janek Rahrt <janek.rahrt@me.com>
2022-08-16 16:56:41 +0200

    feat: add merkle trees
    using pedersen hash

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ export * as json from './utils/json';
 export * as number from './utils/number';
 export * as transaction from './utils/transaction';
 export * as stark from './utils/stark';
+export * as merkle from './utils/merkle';
 export * as ec from './utils/ellipticCurve';
 export * as uint256 from './utils/uint256';
 export * as shortString from './utils/shortString';
