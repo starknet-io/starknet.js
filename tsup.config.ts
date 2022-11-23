8ce1bbf7daeada98e1364e2992b89608204d663a
Janek <janek.rahrt@me.com>
2022-11-23 15:04:18 +0100

    fix: tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   sourcemap: true,
+  outDir: 'dist',
+  dts: {
+    resolve: true,
+  },
   clean: true,
-  format: ['cjs'],
+  format: ['cjs', 'iife', 'esm'],
   globalName: 'starknet',
+  legacyOutput: true,
 });
