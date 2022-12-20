6c5c34090c425f135d1a656efa93106972807160
Dhruv Kelawala <dhruvrk2000@gmail.com>
2022-12-20 10:23:20 +0000

    chore: update package

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -2,7 +2,7 @@
 /* eslint-disable import/extensions */
 import { keccak256 } from 'ethereum-cryptography/keccak.js';
 import { hexToBytes } from 'ethereum-cryptography/utils.js';
-import { keccak, pedersen as micro_pedersen } from 'micro-starknet';
+import { keccak, pedersen as micro_pedersen } from 'micro-curve-definitions/lib/stark';
 
 import { API_VERSION, MASK_250, StarknetChainId, TransactionHashPrefix } from '../constants';
 import { CompiledContract, RawCalldata } from '../types/lib';
