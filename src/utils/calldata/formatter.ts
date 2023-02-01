8a8b00f6891055070dcabe77c1344a887c5e75a7
Toni Tabak <tabaktoni@gmail.com>
2023-02-01 15:06:36 +0100

    refactor: formatter to work on response data, added guards, undefined elements are left as is BN

--- src/utils/calldata/formatter.ts
+++ src/utils/calldata/formatter.ts
@@ -1,31 +1,55 @@
+import { isBN } from 'bn.js';
+
 import { decodeShortString } from '../shortString';
 
+const guard = {
+  isBN: (data: any, type: any, key: any) => {
+    if (!isBN(data[key]))
+      throw new Error(
+        `Data and formatter mismatch on ${key}:${type[key]}, expected response data ${key}:${
+          data[key]
+        } to be BN instead it is ${typeof data[key]}`
+      );
+  },
+  unknown: (data: any, type: any, key: any) => {
+    throw new Error(`Unhandled formatter type on ${key}:${type[key]} for data ${key}:${data[key]}`);
+  },
+};
+
 export default function formatter(data: any, type: any) {
-  return Object.entries(type).reduce((acc, [key, value]) => {
-    if (value === 'string') {
-      acc[key] = decodeShortString(data[key].toString(16));
+  // match data element with type element
+  return Object.entries(data).reduce((acc, [key, value]: [any, any]) => {
+    if (!(key in type)) {
+      // no type definition for element return original element
+      acc[key] = value;
       return acc;
     }
-    if (value === 'number') {
-      acc[key] = data[key].toNumber();
+
+    if (type[key] === 'string') {
+      guard.isBN(data, type, key);
+      acc[key] = decodeShortString(value.toString(16));
       return acc;
     }
-    // object formatter is function
-    if (typeof value === 'function') {
-      acc[key] = value(data[key]);
+    if (type[key] === 'number') {
+      guard.isBN(data, type, key);
+      acc[key] = value.toNumber();
       return acc;
     }
-    if (Array.isArray(value)) {
+    if (typeof type[key] === 'function') {
+      acc[key] = type[key](value);
+      return acc;
+    }
+    if (Array.isArray(type[key])) {
       const arrayObj = formatter(data[key], type[key]);
       acc[key] = Object.values(arrayObj);
       return acc;
     }
-    if (typeof value === 'object') {
+    if (typeof type[key] === 'object') {
       acc[key] = formatter(data[key], type[key]);
       return acc;
     }
 
-    acc[key] = data[key];
+    guard.unknown(data, type, key);
     return acc;
   }, {} as any);
 }
