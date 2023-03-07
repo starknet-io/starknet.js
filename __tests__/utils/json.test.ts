66bc829e1205b7e9f9ccc07ddb5b4f5c4722b987
Petar Penovic <8030344+penovicp@users.noreply.github.com>
2023-03-07 09:48:01 +0100

    refactor: swap json utility dependency

---
+++ __tests__/utils/json.test.ts
@@ -0,0 +1,25 @@
+import { defaultProvider } from '../../src';
+import { parse, parseAlwaysAsBig, stringify } from '../../src/utils/json';
+
+const bi = (x: bigint) => BigInt(Number.MAX_SAFE_INTEGER) + x;
+const objString = `{"constructor":"c","n0":${bi(0n)},"n1":${bi(1n)},"n2":12,"n3":1.1}`;
+
+describe('JSON utility tests', () => {
+  test('stringify', () => {
+    expect(stringify(defaultProvider)).toMatchSnapshot();
+  });
+
+  test('parse', () => {
+    const parsed: any = parse(objString);
+    expect(parsed).toMatchSnapshot();
+    expect(parsed.n0).toEqual(expect.any(Number));
+    expect(parsed.n1).toEqual(expect.any(BigInt));
+  });
+
+  test('parseAlwaysAsBig', () => {
+    const parsed: any = parseAlwaysAsBig(objString);
+    expect(parsed).toMatchSnapshot();
+    expect(parsed.n0).toEqual(expect.any(BigInt));
+    expect(parsed.n1).toEqual(expect.any(BigInt));
+  });
+});
