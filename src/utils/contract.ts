ad9af7f823ef3eeecf7281a94892d34ccef1910a
Toni Tabak <tabaktoni@gmail.com>
2023-03-23 08:56:04 +0100

    chore: contract utils

---
+++ src/utils/contract.ts
@@ -0,0 +1,32 @@
+import { parse } from 'json-bigint';
+
+import { CairoContract } from '../types/lib/contract/index';
+import { CompleteDeclareContractPayload, DeclareContractPayload } from '../types/lib/index';
+import { computeCompiledClassHash, computeContractClassHash } from './hash';
+
+export function isSiera(contract: CairoContract | string) {
+  const compiledContract = typeof contract === 'string' ? parse(contract) : contract;
+  return 'sierra_program' in compiledContract;
+}
+
+export function extractContractHashes(
+  payload: DeclareContractPayload
+): CompleteDeclareContractPayload {
+  const response = { ...payload } as CompleteDeclareContractPayload;
+
+  if (isSiera(payload.contract)) {
+    if (!payload.compiledClassHash && payload.casm) {
+      response.compiledClassHash = computeCompiledClassHash(payload.casm);
+    }
+    if (!response.compiledClassHash)
+      throw new Error(
+        'Extract compiledClassHash failed, provide (CairoAssembly).casm file or compiledClassHash'
+      );
+  }
+
+  response.classHash = payload.classHash ?? computeContractClassHash(payload.contract);
+  if (!response.classHash)
+    throw new Error('Extract classHash failed, provide (CompiledContract).json file or classHash');
+
+  return response;
+}
