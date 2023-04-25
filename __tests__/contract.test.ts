0b20581a1ad5e391d5704fd0cdc10abe21fde166
Toni Tabak <tabaktoni@gmail.com>
2023-04-25 15:49:33 +0200

    test: extend test for contract populate

--- __tests__/contract.test.ts
+++ __tests__/contract.test.ts
@@ -552,15 +552,26 @@ describe('Complex interaction', () => {
     });
 
     test('populate transaction and call with populated data', async () => {
-      const populated = await erc20Echo20Contract.populateTransaction.echo(
+      const args = Object.values(request);
+      // populateTransaction
+      const populated1 = await erc20Echo20Contract.populateTransaction.echo(...args);
+      // populate
+      const populated2 = await erc20Echo20Contract.populate('echo', args);
+      // populate with apply
+      const populated3 = erc20Echo20Contract.populate.apply(erc20Echo20Contract, ['echo', args]);
+      // populateTransaction with compiledCalldata
+      const populated4 = await erc20Echo20Contract.populateTransaction.echo(
         CallData.compile(request)
       );
-      expect(
-        '["474107654995566025798705","123","8","135049554883004558383340439742929429255072943744440858662311072577337126766","203887170123222058415354283980421533276985178030994883159827760142323294308","196343614134218459150194337625778954700414868493373034945803514629145850912","191491606203201332235940470946533476219373216944002683254566549675726417440","150983476482645969577707455338206408996455974968365254240526141964709732462","196916864427988120570407658938236398782031728400132565646592333804118761826","196909666192589839125749789377187946419246316474617716408635151520594095469","2259304674248048077001042434290734","1","1","2","3","4","5","6","1","2","3","4","5","6","7","8","9","10","11","5000","0","1","2","1","2","200","1","2","6","1","2","3","4","5","6","4","1000","0","2000","0","3000","0","4000","0","2","10","11","20","22","2","1","2","3","4","1","2","3","4","3","1","2","3","4","1","2","3","4","1","2","3","4"]'
-      ).toBe(json.stringify(populated.calldata));
+      const expected =
+        '["474107654995566025798705","123","8","135049554883004558383340439742929429255072943744440858662311072577337126766","203887170123222058415354283980421533276985178030994883159827760142323294308","196343614134218459150194337625778954700414868493373034945803514629145850912","191491606203201332235940470946533476219373216944002683254566549675726417440","150983476482645969577707455338206408996455974968365254240526141964709732462","196916864427988120570407658938236398782031728400132565646592333804118761826","196909666192589839125749789377187946419246316474617716408635151520594095469","2259304674248048077001042434290734","1","1","2","3","4","5","6","1","2","3","4","5","6","7","8","9","10","11","5000","0","1","2","1","2","200","1","2","6","1","2","3","4","5","6","4","1000","0","2000","0","3000","0","4000","0","2","10","11","20","22","2","1","2","3","4","1","2","3","4","3","1","2","3","4","1","2","3","4","1","2","3","4"]';
+      expect(expected).toBe(json.stringify(populated1.calldata));
+      expect(expected).toBe(json.stringify(populated2.calldata));
+      expect(expected).toBe(json.stringify(populated3.calldata));
+      expect(expected).toBe(json.stringify(populated4.calldata));
 
       // mark data as compiled (it can be also done manually check defineProperty compiled in CallData.compile)
-      const compiledCallData = CallData.compile(populated.calldata);
+      const compiledCallData = CallData.compile(populated4.calldata);
       const result = await erc20Echo20Contract.echo(compiledCallData, { formatResponse });
 
       // Convert request uint256 to match response
