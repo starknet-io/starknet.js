48625f4a9eaf4e3c294ad840dae78ebab4ab9a12
Petar Penovic <penovicp@gmail.com>
2023-07-06 23:59:50 +0200

    fix: remedy type declaration roll-up issues

--- src/types/api/rpc.ts
+++ src/types/api/rpc.ts
@@ -1,66 +1,64 @@
 import { ADDRESS, CONTRACT_STORAGE_DIFF_ITEM, FELT, OPENRPC } from './openrpc';
 
-export namespace RPC {
-  export type Response = {
-    id: number;
-    jsonrpc: string;
-    result?: any;
-    error?: {
-      code: string;
-      message: string;
-    };
+export type Response = {
+  id: number;
+  jsonrpc: string;
+  result?: any;
+  error?: {
+    code: string;
+    message: string;
   };
+};
 
-  export type ChainId = OPENRPC.CHAIN_ID;
-  export type CallResponse = OPENRPC.CallResponse;
-  export type ContractAddress = ADDRESS;
-  export type Felt = FELT;
-  export type Nonce = OPENRPC.Nonce;
-  export type ContractClass = OPENRPC.ContractClass | OPENRPC.DeprecatedContractClass;
-  export type StateUpdate = OPENRPC.StateUpdate;
-  export type Transaction = OPENRPC.Transaction;
-  export type PendingTransactions = OPENRPC.PendingTransactions;
-  export type TransactionHash = OPENRPC.TransactionHash;
-  export type Trace = OPENRPC.Trace;
-  export type Traces = OPENRPC.Traces;
-  export type BlockHash = OPENRPC.BlockHash;
-  export type BlockHashAndNumber = OPENRPC.BlockHashAndNumber;
-  export type EstimateFeeResponse = OPENRPC.EstimatedFee;
-  export type GetBlockWithTxHashesResponse = OPENRPC.BlockWithTxHashes;
-  export type GetBlockWithTxs = OPENRPC.BlockWithTxs;
-  export type GetStorageAtResponse = OPENRPC.Storage;
-  export type TransactionReceipt = OPENRPC.TransactionReceipt;
-  export type GetTransactionByHashResponse = OPENRPC.Transaction;
-  export type GetTransactionByBlockIdAndIndex = OPENRPC.Transaction;
-  export type GetTransactionCountResponse = number;
-  export type GetBlockNumberResponse = OPENRPC.BlockNumber;
-  export type GetSyncingStatsResponse = OPENRPC.SyncingStatus;
-  export type EventFilter = OPENRPC.EventFilter;
-  export type GetEventsResponse = OPENRPC.Events;
-  export type InvokedTransaction = OPENRPC.InvokedTransaction;
-  export type DeclaredTransaction = OPENRPC.DeclaredTransaction;
-  export type DeployedTransaction = OPENRPC.DeployedTransaction;
-  export type SimulationFlags = OPENRPC.SimulationFlags;
-  export type BroadcastedTransaction = OPENRPC.BroadcastedTransaction;
-  export type EstimatedFee = OPENRPC.EstimatedFee;
-  export type Methods = OPENRPC.Methods;
-  export type Storage = OPENRPC.Storage;
-  export type SimulateTransactionResponse = OPENRPC.SimulatedTransactions;
+export type ChainId = OPENRPC.CHAIN_ID;
+export type CallResponse = OPENRPC.CallResponse;
+export type ContractAddress = ADDRESS;
+export type Felt = FELT;
+export type Nonce = OPENRPC.Nonce;
+export type ContractClass = OPENRPC.ContractClass | OPENRPC.DeprecatedContractClass;
+export type StateUpdate = OPENRPC.StateUpdate;
+export type Transaction = OPENRPC.Transaction;
+export type PendingTransactions = OPENRPC.PendingTransactions;
+export type TransactionHash = OPENRPC.TransactionHash;
+export type Trace = OPENRPC.Trace;
+export type Traces = OPENRPC.Traces;
+export type BlockHash = OPENRPC.BlockHash;
+export type BlockHashAndNumber = OPENRPC.BlockHashAndNumber;
+export type EstimateFeeResponse = OPENRPC.EstimatedFee;
+export type GetBlockWithTxHashesResponse = OPENRPC.BlockWithTxHashes;
+export type GetBlockWithTxs = OPENRPC.BlockWithTxs;
+export type GetStorageAtResponse = OPENRPC.Storage;
+export type TransactionReceipt = OPENRPC.TransactionReceipt;
+export type GetTransactionByHashResponse = OPENRPC.Transaction;
+export type GetTransactionByBlockIdAndIndex = OPENRPC.Transaction;
+export type GetTransactionCountResponse = number;
+export type GetBlockNumberResponse = OPENRPC.BlockNumber;
+export type GetSyncingStatsResponse = OPENRPC.SyncingStatus;
+export type EventFilter = OPENRPC.EventFilter;
+export type GetEventsResponse = OPENRPC.Events;
+export type InvokedTransaction = OPENRPC.InvokedTransaction;
+export type DeclaredTransaction = OPENRPC.DeclaredTransaction;
+export type DeployedTransaction = OPENRPC.DeployedTransaction;
+export type SimulationFlags = OPENRPC.SimulationFlags;
+export type BroadcastedTransaction = OPENRPC.BroadcastedTransaction;
+export type EstimatedFee = OPENRPC.EstimatedFee;
+export type Methods = OPENRPC.Methods;
+export type Storage = OPENRPC.Storage;
+export type SimulateTransactionResponse = OPENRPC.SimulatedTransactions;
 
-  export enum TransactionType {
-    DECLARE = 'DECLARE',
-    DEPLOY = 'DEPLOY',
-    DEPLOY_ACCOUNT = 'DEPLOY_ACCOUNT',
-    INVOKE = 'INVOKE',
-    L1_HANDLER = 'L1_HANDLER',
-  }
-
-  // Exported Diff on Sequencer (can be removed when diff resolved by new RPC v)
-  export type StorageDiffs = Array<CONTRACT_STORAGE_DIFF_ITEM>;
-  export type DeprecatedDeclaredClasses = Array<FELT>;
-  export type Nonces = Array<{
-    contract_address: ADDRESS;
-    nonce: FELT;
-  }>;
-  export type ReplacedClasses = Array<{ contract_address: ADDRESS; class_hash: FELT }>;
+export enum TransactionType {
+  DECLARE = 'DECLARE',
+  DEPLOY = 'DEPLOY',
+  DEPLOY_ACCOUNT = 'DEPLOY_ACCOUNT',
+  INVOKE = 'INVOKE',
+  L1_HANDLER = 'L1_HANDLER',
 }
+
+// Exported Diff on Sequencer (can be removed when diff resolved by new RPC v)
+export type StorageDiffs = Array<CONTRACT_STORAGE_DIFF_ITEM>;
+export type DeprecatedDeclaredClasses = Array<FELT>;
+export type Nonces = Array<{
+  contract_address: ADDRESS;
+  nonce: FELT;
+}>;
+export type ReplacedClasses = Array<{ contract_address: ADDRESS; class_hash: FELT }>;
--- src/utils/json.ts
+++ src/utils/json.ts
@@ -9,12 +9,18 @@ const parseIntAsNumberOrBigInt = (x: string) => {
 };
 // NOTE: the String() wrapping is used so the behaviour conforms to JSON.parse()
 // which can accept simple data types but is not represented in the default typing
-export const parse = (x: string): any => json.parse(String(x), null, parseIntAsNumberOrBigInt);
+export const parse = (x: string): any => json.parse(String(x), undefined, parseIntAsNumberOrBigInt);
 export const parseAlwaysAsBig = (x: string): any =>
-  json.parse(String(x), null, json.parseNumberAndBigInt);
+  json.parse(String(x), undefined, json.parseNumberAndBigInt);
 
 // NOTE: the not-null assertion is used so the return type conforms to JSON.stringify()
 // which can also return undefined but is not represented in the default typing
-export const stringify = (...p: Parameters<typeof json.stringify>): string => json.stringify(...p)!;
+export const stringify = (
+  value: json.JavaScriptValue,
+  replacer?: any,
+  space?: string | number | undefined,
+  numberStringifiers?: json.NumberStringifier[] | undefined
+): string => json.stringify(value, replacer, space, numberStringifiers)!;
+
 /** @deprecated equivalent to 'stringify', alias will be removed */
 export const stringifyAlwaysAsBig = stringify;
