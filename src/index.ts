50cb64cadc9a382a336e20e25bdf65814efca332
Petar Penović <penovicp@gmail.com>
2023-08-22 19:34:46 +0200

    fix: expose event parsing utilities

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,7 @@ export * as ec from './utils/ec';
 export * as starknetId from './utils/starknetId';
 export * as provider from './utils/provider';
 export * as selector from './utils/selector';
+export * as events from './utils/events/index';
 export * from './utils/address';
 export * from './utils/url';
 export * from './utils/calldata';
