99214068d17b620badd560c6e3f2d1677f5b44c5
Toni Tabak <tabaktoni@gmail.com>
2023-10-17 14:07:18 +0200

    test: wallet test placeholder

---
+++ __tests__/walletAccount.test.ts
@@ -0,0 +1,8 @@
+// TODO Mock: get-starknet UI connect/disconnect wallet
+// TODO Create Mock Wallet;
+
+describe('wallet account test', () => {
+  test('estimateInvokeFee Cairo 0', async () => {
+    return true;
+  });
+});
