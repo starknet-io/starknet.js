df73127e431fb328de8bf445630cfac850b592e9
momodaka <463435681@qq.com>
2023-12-01 18:28:13 +0800

    docs: update typo for `polyfill`

--- src/utils/fetchPonyfill.ts
+++ src/utils/fetchPonyfill.ts
@@ -2,4 +2,4 @@ import isomorphicFetch from 'isomorphic-fetch';
 
 export default (typeof window !== 'undefined' && window.fetch) || // use buildin fetch in browser if available
   (typeof global !== 'undefined' && global.fetch) || // use buildin fetch in node, react-native and service worker if available
-  isomorphicFetch; // ponyfill fetch in node and browsers that don't have it
+  isomorphicFetch; // polyfill fetch in node and browsers that don't have it
