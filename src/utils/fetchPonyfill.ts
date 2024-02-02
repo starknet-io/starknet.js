8abaa99ef54db06d817bf1f782b80bdd6511d371
Piotr Piwoński <piwonskp@gmail.com>
2024-02-02 18:32:27 +0000

    feat: use fetch-cookie for sticky sessions

--- src/utils/fetchPonyfill.ts
+++ src/utils/fetchPonyfill.ts
@@ -1,5 +1,6 @@
+import makeFetchCookie from 'fetch-cookie';
 import isomorphicFetch from 'isomorphic-fetch';
 
 export default (typeof window !== 'undefined' && window.fetch) || // use buildin fetch in browser if available
-  (typeof global !== 'undefined' && global.fetch) || // use buildin fetch in node, react-native and service worker if available
+  (typeof global !== 'undefined' && makeFetchCookie(global.fetch)) || // use buildin fetch in node, react-native and service worker if available
   isomorphicFetch; // ponyfill fetch in node and browsers that don't have it
