68cb801d699ce7632211c5e1781d381013ac39c5
Toni Tabak <tt@spaceshard.io>
2024-02-12 11:34:24 +0100

    chore: channel constructor props

---
+++ src/types/channel/index.ts
@@ -0,0 +1,15 @@
+import { NetworkName, StarknetChainId } from '../../constants';
+import { ERPCVersion } from '../api';
+import { BlockIdentifier } from '../lib';
+
+export type RpcChannelOptions = {
+  nodeUrl?: string | NetworkName;
+  retries?: number;
+  headers?: object;
+  blockIdentifier?: BlockIdentifier;
+  chainId?: StarknetChainId;
+  default?: boolean;
+  waitMode?: boolean;
+  rpcVersion?: ERPCVersion;
+  speckVersion?: string;
+};
