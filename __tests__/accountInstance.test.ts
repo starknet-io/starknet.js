83011bed44857d9161e055d1d6d411d8e7532b92
Toni Tabak <tabaktoni@gmail.com>
2024-03-01 16:38:45 -0700

    Update __tests__/accountInstance.test.ts
    Co-authored-by: Petar Penović <pp@spaceshard.io>

--- __tests__/accountInstance.test.ts
+++ __tests__/accountInstance.test.ts
@@ -2,11 +2,8 @@ import { Account, AccountInstance, BigNumberish, LibraryError, RpcProvider } fro
 
 describe('Account instantiated from provider instance', () => {
   const rpc = new RpcProvider();
-
-  const customNonce = async function (contractAddress: BigNumberish) {
-    return `my custom implementation of get nonce for address ${contractAddress}`;
-  };
-  rpc.getNonceForAddress = customNonce;
+  const getNonceMock = jest.fn().mockResolvedValue('mock');
+  rpc.getNonceForAddress = getNonceMock;
 
   test('Account to throw on extended custom methods for using re-instantiation on provider', () => {
     const acc = new Account(rpc, '0x0', '0x1');
@@ -16,13 +13,13 @@ describe('Account instantiated from provider instance', () => {
 
   test('AccountInstance to pass on extending', async () => {
     const acc = new AccountInstance(rpc, '0x0', '0x1');
-    const result = await acc.getNonceForAddress('0x0');
-    expect(result).toBe('my custom implementation of get nonce for address 0x0');
+    expect(await acc.getNonceForAddress('0x1')).toBe('mock');
+    expect(getNonceMock).toHaveBeenLastCalledWith('0x1');
   });
 
   test('Account to pass on extended custom methods for using instance on provider', async () => {
     const acc = new Account(rpc, '0x0', '0x1', undefined, undefined, true);
-    const result = await acc.getNonceForAddress('0x0');
-    expect(result).toBe('my custom implementation of get nonce for address 0x0');
+    expect(await acc.getNonceForAddress('0x2')).toBe('mock');
+    expect(getNonceMock).toHaveBeenLastCalledWith('0x2');
   });
 });
