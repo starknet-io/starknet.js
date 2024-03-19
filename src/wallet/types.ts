ac5267aed7c9989ca98cc6771a13e6f484790a72
Toni Tabak <tabaktoni@gmail.com>
2024-03-19 16:12:04 +0100

    chore: clenup

--- src/wallet/types.ts
+++ src/wallet/types.ts
@@ -3,6 +3,5 @@ import { RpcMessage, StarknetWindowObject } from './getst/main';
 // ---- TT Request Handler
 export type RpcCall = Omit<RpcMessage, 'result'>;
 
-// -- TT Better naming
 // This is provider object expected by WalletAccount to communicate with wallet
 export interface StarknetWalletProvider extends StarknetWindowObject {}
