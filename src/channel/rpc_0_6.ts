c177eefc0c1a746659db94c865165a7fac411ba4
Sergei Volynkin <sergei.volynkin@paradigm.co>
2024-04-02 12:16:00 +0800

    fix: estimateMessageFee should handle `from_address` as checksum'ed eth address

--- src/channel/rpc_0_6.ts
+++ src/channel/rpc_0_6.ts
@@ -18,6 +18,7 @@ import {
   waitForTransactionOptions,
 } from '../types';
 import { JRPC, RPCSPEC06 as RPC } from '../types/api';
+import { getChecksumAddress } from '../utils/address';
 import { CallData } from '../utils/calldata';
 import { isSierra } from '../utils/contract';
 import fetch from '../utils/fetchPonyfill';
@@ -565,7 +566,7 @@ export class RpcChannel {
   ) {
     const { from_address, to_address, entry_point_selector, payload } = message;
     const formattedMessage = {
-      from_address: toHex(from_address),
+      from_address: getChecksumAddress(from_address),
       to_address: toHex(to_address),
       entry_point_selector: getSelector(entry_point_selector),
       payload: getHexStringArray(payload),
