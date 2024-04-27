ac3982ed41250fd46f598153fc2e213fa176020c
Nuelo <akalo.emmanuel18@gmail.com>
2024-04-27 05:49:30 +0100

    chore: add examples to JsDoc for uint256.ts file

--- src/utils/uint256.ts
+++ src/utils/uint256.ts
@@ -10,6 +10,14 @@ export { UINT_128_MAX, UINT_256_MAX };
 /**
  * Convert Uint256 to bigint
  * Legacy support Export
+ * @param uint256 Uint256 value to convert to bigint
+ * @returns {bigint} BigInt representation of the input Uint25
+ * @example
+ * ```typescript
+ * const uint256Value: Uint256 = "0x2fd23d9182193775423497fc0c472e156c57c69e4089a1967fb288a2d84e914";
+ * const result = uint256ToBN(uint256Value);
+ * // result = 28139218908760698114965631183142736658067655190345294133754686837148899816404n
+ * ```
  */
 export function uint256ToBN(uint256: Uint256) {
   return new CairoUint256(uint256).toBigInt();
@@ -18,6 +26,14 @@ export function uint256ToBN(uint256: Uint256) {
 /**
  * Test BigNumberish is smaller or equal 2**256-1
  * Legacy support Export
+ * @param bn BigNumberish value to test
+ * @returns {boolean} True if the input value is smaller or equal to 2**256-1, false otherwise
+ * @example
+ * ```typescript
+ * const bnValue: BigNumberish = 28139218908760698114965631183142736658067655190345294133754686837148899816404n;
+ * const result = isUint256(bnValue);
+ * // result = true
+ * ```
  */
 export function isUint256(bn: BigNumberish): boolean {
   return CairoUint256.is(bn);
@@ -26,6 +42,14 @@ export function isUint256(bn: BigNumberish): boolean {
 /**
  * Convert BigNumberish (string | number | bigint) to Uint256 (hex)
  * Legacy support Export
+ * @param bn BigNumberish value to convert to Uint256 (hex)
+ * @returns {Uint256} Uint256 (hex) representation of the input BigNumberish value
+ * @example
+ * ```typescript
+ * const bnValue: BigNumberish = 28139218908760698114965631183142736658067655190345294133754686837148899816404n;
+ * const result = bnToUint256(bnValue);
+ * // result = "0x2fd23d9182193775423497fc0c472e156c57c69e4089a1967fb288a2d84e914"
+ * ```
  */
 export function bnToUint256(bn: BigNumberish): Uint256 {
   return new CairoUint256(bn).toUint256HexString();
