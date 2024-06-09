2b2d965ba12e7929b472f7cd7e798e3ed7d858c2
Fabián Valverde <fabianvalverde@Fabians-MacBook-Pro.local>
2024-06-09 00:33:54 -0600

    test: add starknet rpc spec json schemas

--- __tests__/config/schema.ts
+++ __tests__/config/schema.ts
@@ -2,14 +2,25 @@ import ajvKeywords from 'ajv-keywords';
 import { matchersWithOptions } from 'jest-json-schema';
 
 import accountSchemas from '../schemas/account.json';
-import componentSchemas from '../schemas/component.json';
+import componentSchemas from '../schemas/rpc/component.json';
+import starknet_api_openrpc from '../schemas/rpc/starknet_api_openrpc.json';
+import starknet_metadata from '../schemas/rpc/starknet_metadata.json';
+import starknet_trace_api_openrpc from '../schemas/rpc/starknet_trace_api_openrpc.json';
+import starknet_write_api from '../schemas/rpc/starknet_write_api.json';
 import libSchemas from '../schemas/lib.json';
 import providerSchemas from '../schemas/provider.json';
 import rpcSchemas from '../schemas/rpc.json';
 import { isBigInt } from '../../src/utils/num';
 
 const matcherSchemas = [accountSchemas, libSchemas, providerSchemas, rpcSchemas];
-const schemas = [...matcherSchemas, componentSchemas];
+const schemas = [
+  ...matcherSchemas,
+  componentSchemas,
+  starknet_api_openrpc,
+  starknet_metadata,
+  starknet_trace_api_openrpc,
+  starknet_write_api,
+];
 const jestJsonMatchers = matchersWithOptions({ schemas }, (ajv: any) => {
   // @ts-ignore
   ajv.addKeyword({
@@ -35,6 +46,10 @@ export const initializeMatcher = (expect: jest.Expect) => {
   });
   expect(accountSchemas).toBeValidSchema();
   expect(componentSchemas).toBeValidSchema();
+  expect(starknet_api_openrpc).toBeValidSchema();
+  expect(starknet_metadata).toBeValidSchema();
+  expect(starknet_trace_api_openrpc).toBeValidSchema();
+  expect(starknet_write_api).toBeValidSchema();
   expect(libSchemas).toBeValidSchema();
   expect(providerSchemas).toBeValidSchema();
   expect(rpcSchemas).toBeValidSchema();
