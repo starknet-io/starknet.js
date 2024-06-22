a29fa18096ff61802887ba50212ed6e7a91d273b
lukasaric <rpg.ari7@gmail.com>
2024-06-22 20:55:22 +0200

    chore: replace tsup with unjs/unbuild

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  sourcemap: true,
-  clean: true,
-  format: ['cjs'],
-  globalName: 'starknet',
-});
