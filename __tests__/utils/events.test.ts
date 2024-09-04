9826b1214c3cdbf67d9ce8b77d93c5f6d8736822
lukasaric <rpg.ari7@gmail.com>
2024-09-04 20:09:24 +0200

    chore: add parse UDC event test

--- __tests__/utils/events.test.ts
+++ __tests__/utils/events.test.ts
@@ -6,9 +6,10 @@ import type {
   CairoEventVariant,
   FunctionAbi,
   InterfaceAbi,
+  InvokeTransactionReceiptResponse,
   RPC,
 } from '../../src';
-import { isAbiEvent, getAbiEvents, parseEvents } from '../../src/utils/events';
+import { isAbiEvent, getAbiEvents, parseEvents, parseUDCEvent } from '../../src/utils/events';
 
 const getAbiEventEntry = (): AbiEntry => ({ name: 'test', type: 'event' });
 
@@ -276,5 +277,73 @@ describe('parseEvents', () => {
 
     abiEvents['0x3c719ce4f57dd2d9059b9ffed65417d694a29982d35b188574144d6ae6c3f87'].name = '';
     expect(() => parseEvents([event], abiEvents, abiStructs, abiEnums)).toBeTruthy();
+  });
+});
+
+describe('parseUDCEvent', () => {
+  test('should return parsed UDC event', () => {
+    const txReceipt: InvokeTransactionReceiptResponse = {
+      type: 'INVOKE',
+      transaction_hash: '0x6eebff0d931f36222268705ca791fd0de8d059eaf01887eecf1ce99a6c27f49',
+      actual_fee: { unit: 'WEI', amount: '0x33d758c09000' },
+      messages_sent: [],
+      events: [
+        {
+          from_address: '0x41a78e741e5af2fec34b695679bc6891742439f7afb8484ecd7766661ad02bf',
+          keys: ['0x26b160f10156dea0639bec90696772c640b9706a47f5b8c52ea1abe5858b34d'],
+          data: [
+            '0x1f1209f331cda3e84202f5495446028cd8730159ab24e08a5fd96125257673f',
+            '0x6cee47a1571f83b30b3549fce4aceda18d2533a51b0016b75a50466c708daad',
+            '0x0',
+            '0x54328a1075b8820eb43caf0caa233923148c983742402dcfc38541dd843d01a',
+            '0x3',
+            '0x546f6b656e',
+            '0x4552433230',
+            '0x6cee47a1571f83b30b3549fce4aceda18d2533a51b0016b75a50466c708daad',
+            '0x76d9fae688efa7dc5defa712c1fa7df537e4c0f5f8b05842a1fd4a6d8d9d3a1',
+          ],
+        },
+        {
+          from_address: '0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+          keys: [
+            '0x99cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9',
+            '0x6cee47a1571f83b30b3549fce4aceda18d2533a51b0016b75a50466c708daad',
+            '0x1000',
+          ],
+
+          data: ['0x33d758c09000', '0x0'],
+        },
+      ],
+      execution_status: 'SUCCEEDED',
+      finality_status: 'ACCEPTED_ON_L2',
+      block_hash: '0xdfc9b788478b2a2b9bcba19ab7d86996bcc45c4f8a865435469334e9077b24',
+      block_number: 584,
+      execution_resources: {
+        steps: 9490,
+        memory_holes: 143,
+        range_check_builtin_applications: 198,
+        pedersen_builtin_applications: 34,
+        ec_op_builtin_applications: 3,
+        data_availability: { l1_gas: 0, l1_data_gas: 544 },
+      },
+    };
+
+    const parsedUDCEvent = parseUDCEvent(txReceipt);
+    const result = {
+      transaction_hash: '0x6eebff0d931f36222268705ca791fd0de8d059eaf01887eecf1ce99a6c27f49',
+      contract_address: '0x1f1209f331cda3e84202f5495446028cd8730159ab24e08a5fd96125257673f',
+      address: '0x1f1209f331cda3e84202f5495446028cd8730159ab24e08a5fd96125257673f',
+      deployer: '0x6cee47a1571f83b30b3549fce4aceda18d2533a51b0016b75a50466c708daad',
+      unique: '0x0',
+      classHash: '0x54328a1075b8820eb43caf0caa233923148c983742402dcfc38541dd843d01a',
+      calldata_len: '0x3',
+      calldata: [
+        '0x546f6b656e',
+        '0x4552433230',
+        '0x6cee47a1571f83b30b3549fce4aceda18d2533a51b0016b75a50466c708daad',
+      ],
+      salt: '0x76d9fae688efa7dc5defa712c1fa7df537e4c0f5f8b05842a1fd4a6d8d9d3a1',
+    };
+    expect(parsedUDCEvent).toStrictEqual(result);
   });
 });
