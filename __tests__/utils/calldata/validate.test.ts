79bbae6636524994a1f754c42eee5d988c346d26
lukasaric <rpg.ari7@gmail.com>
2024-09-06 11:06:52 +0200

    chore: cover more test cases for uint validation

--- __tests__/utils/calldata/validate.test.ts
+++ __tests__/utils/calldata/validate.test.ts
@@ -1,5 +1,11 @@
 import validateFields from '../../../src/utils/calldata/validate';
-import type { AbiEntry, AbiEnums, AbiStructs, FunctionAbi } from '../../../src';
+import {
+  Uint,
+  type AbiEntry,
+  type AbiEnums,
+  type AbiStructs,
+  type FunctionAbi,
+} from '../../../src';
 
 const getAbiEventEntry = (type: string): AbiEntry => ({ name: 'test', type });
 
@@ -131,7 +137,7 @@ describe('validateFields', () => {
     });
 
     test('should throw an error if parameter is not the type of string', () => {
-      const validateFelt = (params: unknown[]) =>
+      const validateBytes31 = (params: unknown[]) =>
         validateFields(
           getFunctionAbi('core::bytes_31::bytes31'),
           params,
@@ -141,16 +147,16 @@ describe('validateFields', () => {
 
       const error = new Error('Validate: arg test should be a string.');
 
-      expect(() => validateFelt([0])).toThrow(error);
-      expect(() => validateFelt([BigInt(22)])).toThrow(error);
-      expect(() => validateFelt([new Map()])).toThrow(error);
-      expect(() => validateFelt([true])).toThrow(error);
-      expect(() => validateFelt([])).toThrow(error);
-      expect(() => validateFelt([Symbol('test')])).toThrow(error);
+      expect(() => validateBytes31([0])).toThrow(error);
+      expect(() => validateBytes31([BigInt(22)])).toThrow(error);
+      expect(() => validateBytes31([new Map()])).toThrow(error);
+      expect(() => validateBytes31([true])).toThrow(error);
+      expect(() => validateBytes31([])).toThrow(error);
+      expect(() => validateBytes31([Symbol('test')])).toThrow(error);
     });
 
     test('should throw an error if parameter is less than 32 chars', () => {
-      const validateFelt = (params: unknown[]) =>
+      const validateBytes31 = (params: unknown[]) =>
         validateFields(
           getFunctionAbi('core::bytes_31::bytes31'),
           params,
@@ -161,7 +167,104 @@ describe('validateFields', () => {
       const error = new Error(
         'Validate: arg test cairo typed core::bytes_31::bytes31 should be a string of less than 32 characters.'
       );
-      expect(() => validateFelt(['String_that_is_bigger_than_32_characters'])).toThrow(error);
+      expect(() => validateBytes31(['String_that_is_bigger_than_32_characters'])).toThrow(error);
+    });
+  });
+
+  describe('Uint validation', () => {
+    test('should return void if Uint "u8" validation passes', () => {
+      const result = validateFields(
+        getFunctionAbi(Uint.u8),
+        [255n],
+        getAbiStructs(),
+        getAbiEnums()
+      );
+      expect(result).toBeUndefined();
+    });
+
+    test('should return void if Uint "u16" validation passes', () => {
+      const result = validateFields(
+        getFunctionAbi(Uint.u16),
+        [65535n],
+        getAbiStructs(),
+        getAbiEnums()
+      );
+      expect(result).toBeUndefined();
+    });
+
+    test('should return void if Uint "u32" validation passes', () => {
+      const result = validateFields(
+        getFunctionAbi(Uint.u32),
+        [4294967295n],
+        getAbiStructs(),
+        getAbiEnums()
+      );
+      expect(result).toBeUndefined();
+    });
+
+    test('should return void if Uint "u64" validation passes', () => {
+      const result = validateFields(
+        getFunctionAbi(Uint.u64),
+        [2n ** 64n - 1n],
+        getAbiStructs(),
+        getAbiEnums()
+      );
+      expect(result).toBeUndefined();
+    });
+
+    test('should return void if Uint "u128" validation passes', () => {
+      const result = validateFields(
+        getFunctionAbi(Uint.u128),
+        [2n ** 128n - 1n],
+        getAbiStructs(),
+        getAbiEnums()
+      );
+      expect(result).toBeUndefined();
+    });
+
+    test('should return void if Uint "u256" validation passes', () => {
+      const result = validateFields(
+        getFunctionAbi(Uint.u256),
+        [2n ** 256n - 1n],
+        getAbiStructs(),
+        getAbiEnums()
+      );
+      expect(result).toBeUndefined();
+    });
+
+    test('should return void if Uint "u512" validation passes', () => {
+      const result = validateFields(
+        getFunctionAbi(Uint.u512),
+        [2n ** 512n - 1n],
+        getAbiStructs(),
+        getAbiEnums()
+      );
+      expect(result).toBeUndefined();
+    });
+
+    test('should throw an error if parameter is too large', () => {
+      const validateUint = (params: unknown[]) =>
+        validateFields(getFunctionAbi(Uint.u8), params, getAbiStructs(), getAbiEnums());
+
+      const error = new Error(
+        'Validation: Parameter is to large to be typed as Number use (BigInt or String)'
+      );
+
+      expect(() => validateUint([Number.MAX_SAFE_INTEGER + 1])).toThrow(error);
+    });
+
+    test('should throw an error if parameter type is not valid', () => {
+      const validateUint = (params: unknown[]) =>
+        validateFields(getFunctionAbi(Uint.u8), params, getAbiStructs(), getAbiEnums());
+
+      const getError = (param: any) =>
+        new Error(
+          `Validate: arg test of cairo type ${Uint.u8} should be type (String, Number or BigInt), but is ${typeof param} ${param}.`
+        );
+
+      expect(() => validateUint([new Map()])).toThrow(getError(new Map()));
+      expect(() => validateUint([true])).toThrow(getError(true));
+      expect(() => validateUint([{ test: 'test' }])).toThrow(getError({ test: 'test' }));
     });
   });
 });
