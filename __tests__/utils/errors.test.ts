947e8b457d08a7533139072b911ace4e382988ae
Petar Penović <pp@spaceshard.io>
2024-10-24 11:52:05 +0200

    feat: increase rpc error information propagation (#1213)

---
+++ __tests__/utils/errors.test.ts
@@ -0,0 +1,22 @@
+import { RPC, RpcError } from '../../src';
+
+describe('Error utility tests', () => {
+  test('RpcError', () => {
+    const baseError: RPC.Errors.UNEXPECTED_ERROR = {
+      code: 63,
+      message: 'An unexpected error occurred',
+      data: 'data',
+    };
+    const method = 'GET';
+    const error = new RpcError(baseError, method, method);
+
+    expect(error.baseError).toBe(baseError);
+    expect(error.message).toMatch(/^RPC: \S+ with params \S+/);
+    expect(error.code).toEqual(baseError.code);
+    expect(error.request.method).toEqual(method);
+    expect(error.request.params).toEqual(method);
+
+    expect(error.isType('BLOCK_NOT_FOUND')).toBe(false);
+    expect(error.isType('UNEXPECTED_ERROR')).toBe(true);
+  });
+});
--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -1,3 +1,8 @@
+/* eslint-disable max-classes-per-file */
+import { RPC, RPC_ERROR, RPC_ERROR_SET } from '../../types';
+import { stringify } from '../json';
+import rpcErrors from './rpc';
+
 // eslint-disable-next-line max-classes-per-file
 export function fixStack(target: Error, fn: Function = target.constructor) {
   const { captureStackTrace } = Error as any;
@@ -36,20 +41,38 @@ export class CustomError extends Error {
 
 export class LibraryError extends CustomError {}
 
-export class GatewayError extends LibraryError {
-  constructor(
-    message: string,
-    public errorCode: string
-  ) {
-    super(message);
-  }
-}
+export class RpcError<BaseErrorT extends RPC_ERROR = RPC_ERROR> extends LibraryError {
+  public readonly request: {
+    method: string;
+    params: any;
+  };
 
-export class HttpError extends LibraryError {
   constructor(
-    message: string,
-    public errorCode: number
+    public readonly baseError: BaseErrorT,
+    method: string,
+    params: any
   ) {
-    super(message);
+    // legacy message format
+    super(`RPC: ${method} with params ${stringify(params, null, 2)}\n
+      ${baseError.code}: ${baseError.message}: ${stringify((baseError as RPC.JRPC.Error).data)}`);
+
+    this.request = { method, params };
+  }
+
+  public get code() {
+    return this.baseError.code;
+  }
+
+  /**
+   * Verifies the underlying RPC error, also serves as a type guard for the _baseError_ property
+   * @example
+   * ```typescript
+   * SomeError.isType('UNEXPECTED_ERROR');
+   * ```
+   */
+  public isType<N extends keyof RPC_ERROR_SET, C extends RPC_ERROR_SET[N]['code']>(
+    typeName: N
+  ): this is RpcError<RPC_ERROR_SET[N] & { code: C }> {
+    return rpcErrors[typeName] === this.code;
   }
 }
