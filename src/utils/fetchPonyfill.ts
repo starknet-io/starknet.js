2cb13327ffe669000e8152b509d14e060b3ea73b
Petar Penović <pp@spaceshard.io>
2025-01-07 15:15:20 +0100

    fix: repair fetch utility for browser environments (#1293)
    * fix: limit fetch dependencies to patch versions
    
    * fix: bind window in fetch utility for browser environments

--- src/utils/fetchPonyfill.ts
+++ src/utils/fetchPonyfill.ts
@@ -5,6 +5,6 @@ import isomorphicFetch from 'isomorphic-fetch';
 import { IS_BROWSER } from './encode';
 import { isUndefined } from './typed';
 
-export default (IS_BROWSER && window.fetch) || // use built-in fetch in browser if available
+export default (IS_BROWSER && window.fetch.bind(window)) || // use built-in fetch in browser if available
   (!isUndefined(global) && makeFetchCookie(global.fetch)) || // use built-in fetch in node, react-native and service worker if available
   isomorphicFetch; // ponyfill fetch in node and browsers that don't have it
