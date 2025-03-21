9a57dafce473432df7040f68e0824e9e78f6e215
Petar Penovic <pp@spaceshard.io>
2025-03-21 09:56:06 +0100

    feat: drop fetch-cookie dependency

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,12 +1,9 @@
-// the ts-ignore suppresses an esm to cjs import error that is resolved with entry point resolution
-// @ts-ignore
-import makeFetchCookie from 'fetch-cookie';
 import { IS_BROWSER } from './encode';
 import { LibraryError } from './errors';
 import { isUndefined } from './typed';
 
 export default (IS_BROWSER && window.fetch.bind(window)) || // use built-in fetch in browser if available
-(!isUndefined(global) && makeFetchCookie(global.fetch)) || // use built-in fetch in node, react-native and service worker if available
+  (!isUndefined(global) && global.fetch) || // use built-in fetch in node, react-native and service worker if available
   // throw with instructions when no fetch is detected
   ((() => {
     throw new LibraryError(
