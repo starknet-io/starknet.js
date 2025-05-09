8d1f0b5e18fda1bb1eb2da821286600b5ee25151
Eduard Bardají Puig <ebardaji@gmail.com>
2025-05-09 10:51:57 +0100

    fix: update fetch checks

--- src/utils/connect/fetch.ts
+++ src/utils/connect/fetch.ts
@@ -1,9 +1,8 @@
 import { LibraryError } from '../errors';
 
-export default (typeof fetch !== 'undefined' && fetch) ||
-  (typeof globalThis !== 'undefined' && globalThis.fetch) ||
+export default (typeof globalThis !== 'undefined' && globalThis.fetch.bind(globalThis)) ||
   (typeof window !== 'undefined' && window.fetch.bind(window)) ||
-  (typeof global !== 'undefined' && global.fetch) ||
+  (typeof global !== 'undefined' && global.fetch.bind(global)) ||
   ((() => {
     throw new LibraryError(
       "'fetch()' not detected, use the 'baseFetch' constructor parameter to set it"
