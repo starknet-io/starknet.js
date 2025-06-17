34e16ec040ec86f5255d32dbdfc7a2511f05724b
Toni Tabak <tabaktoni@gmail.com>
2025-06-17 04:08:05 -0600

    Upgrade WebSocketChannel DX and Buffer (#1416)
    * chore: vip wp fix
    
    * chore: wp2
    
    * feat: improve results
    
    * feat: improve subscription model to Rx
    
    * feat: improve subscription model to Rx with New Class
    
    * docs: documentation update and clenup
    
    * chore: test clenup
    
    * fix: safe on... events for raw dev overide, expose ws options type
    
    * fix: ws parse robusts, unsubrscribe cleanup, data types
    
    * feat: the One Big Beautiful WebScoket
    
    * test: subscription Restoration, Queuing for subscribes
    
    * fix: cleanup, unit and tests, docs, errors
    
    * chore: debug & details

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -76,3 +76,25 @@ export class RpcError<BaseErrorT extends RPC_ERROR = RPC_ERROR> extends LibraryE
     return rpcErrors[typeName] === this.code;
   }
 }
+
+/**
+ * Thrown when a WebSocket request does not receive a response within the configured timeout period.
+ * @property {string} name - The name of the error, always 'TimeoutError'.
+ */
+export class TimeoutError extends LibraryError {
+  constructor(message: string) {
+    super(message);
+    this.name = 'TimeoutError';
+  }
+}
+
+/**
+ * Thrown when an operation is attempted on a WebSocket that is not connected.
+ * @property {string} name - The name of the error, always 'WebSocketNotConnectedError'.
+ */
+export class WebSocketNotConnectedError extends LibraryError {
+  constructor(message: string) {
+    super(message);
+    this.name = 'WebSocketNotConnectedError';
+  }
+}
