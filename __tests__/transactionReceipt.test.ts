c6efd5f276cdbb8964842c2f817bbd88a86d9ccd
Toni Tabak <tabaktoni@gmail.com>
2025-08-18 18:21:18 +0200

    test: transaction  receipt helper ERROR and else variants

--- __tests__/transactionReceipt.test.ts
+++ __tests__/transactionReceipt.test.ts
@@ -8,6 +8,8 @@ import {
   ProviderInterface,
   Account,
   EstimateFeeResponseOverhead,
+  createTransactionReceipt,
+  GetTxReceiptResponseWithoutHelper,
 } from '../src';
 import { contracts } from './config/fixtures';
 import { createTestProvider, getTestAccount } from './config/fixturesInit';
@@ -110,6 +112,66 @@ describe('Transaction receipt utility - RPC 0.8+ - V3', () => {
     expect(isSuccess).toBe(true);
   });
 
+  test('Test else _ case', async () => {
+    const myCall: Call = contract.populate('test_fail', { p1: 10 }); // reverted if not 100
+    const estim: EstimateFeeResponseOverhead = await account.estimateInvokeFee(
+      contract.populate('test_fail', { p1: 100 })
+    );
+    const res = await account.execute(myCall, { ...estim }); // maxFee needed to not throw error in getEstimateFee
+    const txR = await provider.waitForTransaction(res.transaction_hash);
+    expect(txR.value).toHaveProperty('execution_status', TransactionExecutionStatus.REVERTED);
+    expect(txR.statusReceipt).toBe('REVERTED');
+    expect(txR.isSuccess()).toBe(false);
+    expect(txR.isReverted()).toBe(true);
+    expect(txR.isError()).toBe(false);
+    let isReverted: boolean = false;
+    txR.match({
+      SUCCEEDED: (_resp: SuccessfulTransactionReceiptResponse) => {
+        isReverted = false;
+      },
+      _: () => {
+        isReverted = true;
+      },
+    });
+    expect(isReverted).toBe(true);
+  });
+
+  test('Mock false rpc response status for ERROR case', async () => {
+    const estimate = await account.estimateDeployFee({ classHash: dd.declare.class_hash });
+    const res = await account.deployContract(
+      { classHash: dd.declare.class_hash },
+      {
+        resourceBounds: estimate.resourceBounds,
+      }
+    ); // maxFee needed to not throw error in getEstimateFee
+
+    // Create a mock transaction receipt with a non-existent status
+    const receiptWoHelper = (await provider.channel.waitForTransaction(
+      res.transaction_hash
+    )) as GetTxReceiptResponseWithoutHelper;
+    const faleReceipt = {
+      ...receiptWoHelper,
+      execution_status: 'NONEXISTING' as TransactionExecutionStatus,
+    };
+    const txR = createTransactionReceipt(faleReceipt as any);
+
+    expect(txR.statusReceipt).toBe('ERROR');
+    expect(txR.isSuccess()).toBe(false);
+    expect(txR.isReverted()).toBe(false);
+    expect(txR.isError()).toBe(true);
+
+    let isSuccess: boolean = false;
+    txR.match({
+      SUCCEEDED: (_resp: SuccessfulTransactionReceiptResponse) => {
+        isSuccess = true;
+      },
+      _: () => {
+        isSuccess = false;
+      },
+    });
+    expect(isSuccess).toBe(false);
+  });
+
   xtest('test error case', async () => {
     // TODO: this should not be possible as fetch would throw on error before it could be read by Helper
     const txR = await provider.getTransactionReceipt('0x123');
@@ -117,5 +179,15 @@ describe('Transaction receipt utility - RPC 0.8+ - V3', () => {
     expect(txR.isSuccess()).toBe(false);
     expect(txR.isReverted()).toBe(false);
     expect(txR.isError()).toBe(true);
+    let isSuccess: boolean = false;
+    txR.match({
+      SUCCEEDED: (_resp: SuccessfulTransactionReceiptResponse) => {
+        isSuccess = true;
+      },
+      _: () => {
+        isSuccess = false;
+      },
+    });
+    expect(isSuccess).toBe(true);
   });
 });
