1c8df7220153e4e68ef2fe722177f2ba81ffd8c3
Toni Tabak <tabaktoni@gmail.com>
2025-10-17 16:41:27 +0200

    fix: tipos

--- src/utils/connect/blake.ts
+++ src/utils/connect/blake.ts
@@ -1,6 +1,6 @@
 import { blake2s } from '@noble/hashes/blake2s';
 import { config } from '../../global/config';
 
-export default function blakeHahs(uint8Array: Uint8Array): Uint8Array {
+export default function blakeHash(uint8Array: Uint8Array): Uint8Array {
   return config.get('blake')?.(uint8Array) || blake2s(uint8Array, { dkLen: 32 });
 }
