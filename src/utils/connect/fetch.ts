68c1e3040aa731f45ac886bba1c6c2a5c339187b
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2025-11-14 11:02:49 +0100

    use two-steps undefined check

--- src/utils/connect/fetch.ts
+++ src/utils/connect/fetch.ts
@@ -1,8 +1,8 @@
 import { LibraryError } from '../errors';
 
-export default (typeof globalThis?.fetch !== 'undefined' && globalThis.fetch.bind(globalThis)) ||
-  (typeof window?.fetch !== 'undefined' && window.fetch.bind(window)) ||
-  (typeof global?.fetch !== 'undefined' && global.fetch.bind(global)) ||
+export default (typeof globalThis !== 'undefined' && typeof globalThis.fetch !== 'undefined' && globalThis.fetch.bind(globalThis)) ||
+  (typeof window !== 'undefined' && typeof window.fetch !== 'undefined' && window.fetch.bind(window)) ||
+  (typeof global !== 'undefined' && typeof global.fetch !== 'undefined' && global.fetch.bind(global)) ||
   ((() => {
     throw new LibraryError(
       "'fetch()' not detected, use the 'baseFetch' constructor parameter to set it"
