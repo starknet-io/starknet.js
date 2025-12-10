d949328a8c54100b7271022f13f37a9bf048ed35
Toni Tabak <tabaktoni@gmail.com>
2025-12-10 19:25:50 +0100

    test: fix one more mock test with events

--- __tests__/cairo1v2.test.ts
+++ __tests__/cairo1v2.test.ts
@@ -1005,6 +1005,8 @@ describe('Cairo 1', () => {
           block_hash: '0x39f27ab4cd508ab99e818512b261a7e4ae01072eb4ec8bb86aeb64755f99f2c',
           block_number: 69198,
           transaction_hash: '0x4e38fcce79c115b6fe2c486e3514efc1bd4da386b91c104e97230177d0bf181',
+          transaction_index: 0,
+          event_index: 0,
         },
       ]);
       // From component `DepositComponent`, event `Deposit` (same event name than next)
@@ -1065,6 +1067,8 @@ describe('Cairo 1', () => {
           block_hash: '0x31afd649a5042cb1855ce820708a555eab62fe6ea07a2a538fa9100cdc80383',
           block_number: 69198,
           transaction_hash: '0x7768860d79bfb4c8463d215abea3c267899e373407c6882077f7447051c50de',
+          transaction_index: 0,
+          event_index: 0,
         },
       ]);
       const parsedEventNestedDeposit2 = events.parseEvents(
@@ -1085,6 +1089,8 @@ describe('Cairo 1', () => {
           block_hash: '0x39f27ab4cd508ab99e818512b261a7e4ae01072eb4ec8bb86aeb64755f99f2c',
           block_number: 69198,
           transaction_hash: '0x2d5210e5334a83306abe6f7f5e7e65cd1feed72ad3b8e359a2f4614fa948e1d',
+          transaction_index: 0,
+          event_index: 0,
         },
       ]);
 
@@ -1120,6 +1126,8 @@ describe('Cairo 1', () => {
           block_hash: '0x39f27ab4cd508ab99e818512b261a7e4ae01072eb4ec8bb86aeb64755f99f2c',
           block_number: 69198,
           transaction_hash: '0x2da31a929a9848e9630906275a75a531e1718d4830501e10b0bccacd55f6fe0',
+          transaction_index: 0,
+          event_index: 0,
         },
       ]);
     });
